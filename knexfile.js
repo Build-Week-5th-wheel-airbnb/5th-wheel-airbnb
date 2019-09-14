c845e63b6bf2f70e52b148c743d0a16c3657c7ec
Jeremy Morales <jeremy.morales@gmail.com>
2019-09-14 11:27:33 -0700

    pg test

--- knexfile.js
+++ knexfile.js
@@ -5,13 +5,7 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './dev.sqlite3'
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      }
-    },
-    useNullAsDefault: true
+    }
   },
 
   staging: {
@@ -31,14 +25,8 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    useNullAsDefault: true,
-
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
